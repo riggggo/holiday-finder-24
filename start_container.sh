ec477c1d9e434e6adf06ded72df64c57ca2cbd6b
Rico Finkbeiner <finkbeinerrico@gmail.com>
2022-09-20 13:36:19 +0200

    create start_container.sh

--- start_container.sh
+++ start_container.sh
@@ -2,3 +2,10 @@
 # docker run website_2.0
 
 docker compose up
+
+
+https://stackoverflow.com/questions/32612650/how-to-get-docker-compose-to-always-re-create-containers-from-fresh-images
+#docker-compose pull && docker-compose up -d
+
+
+#docker-compose build --no-cache
