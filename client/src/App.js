cbbd51cf44fa92709ccbc6fec90dd11ee5ef0ea5
Rico Finkbeiner <finkbeinerrico@gmail.com>
2022-09-21 12:25:17 +0200

    update App.js

--- client/src/App.js
+++ client/src/App.js
@@ -5,17 +5,17 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
+        
         <p>
-          Edit <code>src/App.js</code> and save to reload. Test
+        This page is still in development.
         </p>
         <a
           className="App-link"
-          href="https://reactjs.org"
+          href="https://github.com/riggggo/personal-site2.0.git"
           target="_blank"
           rel="noopener noreferrer"
         >
-          Learn React
+          GitHub
         </a>
       </header>
     </div>
