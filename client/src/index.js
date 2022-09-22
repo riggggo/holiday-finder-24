7d3707811d827ee98fb066ba2208936be57c013b
Rico Finkbeiner <finkbeinerrico@gmail.com>
2022-09-22 15:41:38 +0200

    fix root dir

--- client/src/index.js
+++ client/src/index.js
@@ -1,16 +1,16 @@
-import React from 'react';
-import ReactDOM from 'react-dom/client';
-import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
-import { BrowserRouter } from 'react-router-dom';
-const root = ReactDOM.createRoot(document.getElementById('root'));
+import React from "react";
+import ReactDOM from "react-dom/client";
+import "./index.css";
+import App from "./App";
+import reportWebVitals from "./reportWebVitals";
+import { BrowserRouter as Router, Route } from "react-router-dom";
+const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
-  <BrowserRouter basename="/webapp">
   <React.StrictMode>
-    <App />
+    <Router basename={'/website2'}>
+      <Route path="/" element={<App />} />
+    </Router>
   </React.StrictMode>
-  </BrowserRouter>
 );
 
 // If you want to start measuring performance in your app, pass a function
