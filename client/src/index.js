0a17f27d2cf50f56a2eab97099ea50b71bfc57ae
Rico Finkbeiner <finkbeinerrico@gmail.com>
2022-09-23 17:25:53 +0200

    try to fix routing

--- client/src/index.js
+++ client/src/index.js
@@ -3,15 +3,15 @@ import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
 import reportWebVitals from "./reportWebVitals";
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { HashRouter, Route, Routes } from "react-router-dom";
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(
   <React.StrictMode>
-    <BrowserRouter basename="/website2">
+    <HashRouter basename="/website2">
       <Routes>
         <Route path="/" element={<App />} />
       </Routes>
-    </BrowserRouter>
+    </HashRouter>
   </React.StrictMode>
 );
 
