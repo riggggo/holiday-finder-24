65989de6ae9bc95a070ba4b08efefd5353b7c08e
Rico Finkbeiner <finkbeinerrico@gmail.com>
2022-11-01 13:55:42 +0100

    config api

--- api/app.js
+++ api/app.js
@@ -1,19 +1,21 @@
 const http = require('http');
 const cors = require('cors');
 const express = require('express');
-
+const app = express();
 
 const hostname = '127.0.0.1';
 const port = 8080;
+const airports = [
+  "Munich", "Vienna", "Cologne"
+]
 
-const server = http.createServer((req, res) => {
-  res.statusCode = 200;
-  res.setHeader('Content-Type', 'text/plain');
-  res.end('Hello World');
-});
 
-server.listen(port, hostname, () => {
+app.get("/getAirports", (req, res) => {
+  res.json({"airports": airports});
+})
+
+app.listen(port, hostname, () => {
   console.log(`Server running at http://${hostname}:${port}/`);
-});
+})
 
 /*app.use(cors())*/
