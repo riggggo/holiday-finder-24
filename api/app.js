6d29b35a675e7b4c6c8707e9a9f380faee4792d1
Rico Finkbeiner <finkbeinerrico@gmail.com>
2022-11-01 18:18:49 +0100

    debugging

--- api/app.js
+++ api/app.js
@@ -2,7 +2,7 @@ const http = require('http');
 const express = require('express');
 const app = express();
 
-
+app.keepAliveTimeout = 2 * 60 * 1000;
 const port = 8000;
 const airports = [
   "Munich", "Vienna", "Cologne"
