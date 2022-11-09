7079e1c18e5b203461bd1637512474eaba92ce25
Rico Finkbeiner <finkbeinerrico@gmail.com>
2022-11-09 16:38:52 +0100

    update database configuration

--- api/database.js
+++ api/database.js
@@ -1,12 +1,12 @@
 const mysql = require("mysql2");
-require('dotenv').config();
+const password = process.env.DB_PASSWORD;
 
 module.exports = mysql.createConnection({
     connectionLimit: 10,
     host: '127.0.0.1',
     port: '3306',
-    user: 'root',
-    password: process.env.DB_PASSWORD,
+    user: 'rico',
+    password: password,
     database: 'holiday',
     
 })//socketPath: '/var/run/mysqld/mysqld.sock'
