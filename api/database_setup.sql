718172e8cff2ee675eff614059e7763687db6d65
Rico Finkbeiner <90137935+riggggo@users.noreply.github.com>
2023-09-06 12:49:35 +0200

    Update database_setup.sql

--- api/database_setup.sql
+++ api/database_setup.sql
@@ -1,15 +1,11 @@
 /*rsync -avzh hotels.csv  root@x.x.x.x:/root/
 rsync -avzh offers.csv  root@x.x.x.x:/root/
-
-install mysql
 */
 
-CREATE DATABASE holiday_test3; 
-use holiday_test3;
+CREATE DATABASE holiday; 
+use holiday;
 CREATE TABLE hotels (id SMALLINT not null, hotelname VARCHAR (255), latitude FLOAT, longitude FLOAT, hotelstars FLOAT, Primary key (id));
 
-
--- SHOW VARIABLES LIKE "secure_file_priv";
 
 load data infile "/var/lib/mysql-files/hotels.csv" into table hotels COLUMNS TERMINATED BY ','     OPTIONALLY
 ENCLOSED BY '"'     ESCAPED BY '"'     LINES TERMINATED BY '\n'     IGNORE 1 LINES;
@@ -62,6 +58,9 @@ load data infile "/var/lib/mysql-files/offers.csv" into table offers COLUMNS TER
 ENCLOSED BY '"'     ESCAPED BY '"'     LINES TERMINATED BY '\n'     IGNORE 1 LINES;
 
 
+
+create index id_index on hotels(id)
+create index full_index on offers (hotelid, countadults, countchildren, outbounddepartureairport, departuredate, returndate)
 
 create index id_index on hotels(id)
 create index full_index on offers (hotelid, countadults, countchildren, outbounddepartureairport, departuredate, returndate)
